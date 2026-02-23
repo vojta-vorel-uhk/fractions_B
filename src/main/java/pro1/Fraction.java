371d0b8f9e998ffa2369f867c56192acc8dd522c
vorelvo2 <vorelvo2@uhk.cz>
2026-02-23 16:04:15 +0100

    Po cviku 3

--- src/main/java/pro1/Fraction.java
+++ src/main/java/pro1/Fraction.java
@@ -6,12 +6,46 @@ public class Fraction
     private long d;
 
     public Fraction(long n, long d) {
-        this.n = n;
-        this.d = d;
+        var gcd = NumericUtils.gcd(n,d);
+        this.n = n / gcd;
+        this.d = d / gcd;
+    }
+
+    public Fraction add(Fraction b){
+        var sumN = n * b.d + b.n * d;
+        var sumD = d * b.d;
+        return new Fraction(
+                sumN,
+                sumD
+        );
+
     }
 
     @Override
     public String toString() {
         return n + " / "+d;
+    }
+
+    public static Fraction parse(String s)
+    {
+        String[] split = s.split("\\+");
+        var sum = new Fraction(0,1);
+        for(var part : split){
+            Fraction f;
+            if(part.contains("%")){
+                var trimmed=part.replace("%","").trim();
+                f = new Fraction(
+                        Long.parseLong(trimmed),
+                        100);
+            } else{
+                var split2 = part.split("/");
+                f = new Fraction(
+                        Long.parseLong(split2[0].trim()),
+                        Long.parseLong(split2[1].trim())
+                        );
+            }
+            sum = sum.add(f);
+        }
+        return sum;
     }
 }
